45b39a58bf7490995e569c42358826d6e3e3fc8e
David Espino <davidespinog@gmail.com>
2023-07-03 13:26:34 -0600

    Add user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:correct_user) {User.create name: 'David', email: 'david@mail.com', password: 'password'}
+  let(:invalid_user) {User.create email: 'david@mail.com', password: 'password'}
+  let(:invalid_user_two) {User.create name:'david', password: 'password'}
+
+  it 'should be valid if all the required data is given' do 
+    expect(correct_user).to be_valid
+  end
+  it 'should be invalid if the name is not given' do 
+    expect(invalid_user).not_to be_valid
+  end
+  it 'should be invalid if the email is not given' do 
+    expect(invalid_user_two).not_to be_valid
+  end
 end
