18c12194138374946655124cf967b47f6baa63df
David Espino <davidespinog@gmail.com>
2023-07-04 10:46:37 -0600

    Add styles to categories index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
-  resources :categories, only: [:index]
+  resources :categories, only: [:index, :new]
   devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
