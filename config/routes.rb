b507cab9d029fc00146d0bdedf6286ca21def951
David Espino <davidespinog@gmail.com>
2023-07-04 15:18:36 -0600

    Add nested route for operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
-  resources :categories, only: [:index, :new, :create]
+  # resources :categories, only: [:index, :new, :create]
   devise_for :users
-
+  resources :categories, only: [:index, :new, :create] do
+    resources :operations, only: [:index, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root to: 'home#index'
