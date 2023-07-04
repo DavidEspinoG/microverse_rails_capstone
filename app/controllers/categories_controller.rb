a1f5dffe7f3d103010884f5c730a63a1b1e2ae68
David Espino <davidespinog@gmail.com>
2023-07-04 14:23:50 -0600

    Add styles to categories view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,19 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @categories = Category.all
+  end
 
-  def new 
+  def new
     @category = Category.new
   end
 
-  def create 
+  def create
     @category = Category.new name: params[:name], icon: params[:icon], user: current_user
     if @category.save
       redirect_to categories_path
     else
       flash.alert = @category.errors.full_messages
-      redirect_back(fallback_location: '/')
+      redirect_back(fallback_location: root_path)
     end
   end
-end 
+end
