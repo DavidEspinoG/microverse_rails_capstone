9dea59451fe284c21d11963b2b0229eaafb321c9
David Espino <davidespinog@gmail.com>
2023-07-04 19:53:51 -0600

    Fix bug in at least one category validation

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -10,18 +10,21 @@ class OperationsController < ApplicationController
   end
 
   def create
-    @operation = Operation.new name: params[:name], amount: params[:amount],
-                               author: current_user
-    flash.alert = 'Choose at least one category'
-    redirect_back(fallback_location: root_path)
-    if @operation.save
-      params[:category].each do |e|
-        category = Category.find(e)
-        @operation.categories << category
+    if params[:category]
+      @operation = Operation.new name: params[:name], amount: params[:amount],
+                                author: current_user
+      if @operation.save
+        params[:category].each do |e|
+          category = Category.find(e)
+          @operation.categories << category
+        end
+        redirect_to root_path
+      else
+        flash.alert = @operation.errors.full_messages
+        redirect_back(fallback_location: root_path)
       end
-      redirect_to root_path
     else
-      flash.alert = @operation.errors.full_messages
+      flash.alert = 'Choose at least one category'
       redirect_back(fallback_location: root_path)
     end
   end
