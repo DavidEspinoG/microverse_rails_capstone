f6d14331d8f88441a839785da9e2a701c3ae4441
David Espino <davidespinog@gmail.com>
2023-07-04 19:35:18 -0600

    Required category to create transaction

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -10,6 +10,19 @@ class OperationsController < ApplicationController
   end
 
   def create
-    puts params
+    @operation = Operation.new name: params[:name], amount: params[:amount],
+      author: current_user
+    flash.alert = 'Choose at least one category'
+    redirect_back(fallback_location: root_path)
+    if @operation.save
+        params[:category].each do |e|
+          category = Category.find(e)
+          @operation.categories << category
+        end
+      redirect_to root_path
+    else
+      flash.alert = @operation.errors.full_messages
+      redirect_back(fallback_location: root_path) 
+    end
   end
 end
