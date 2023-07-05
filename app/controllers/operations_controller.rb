a6a02858e07313e916ffda182f837977b7c2933a
David Espino <davidespinog@gmail.com>
2023-07-04 19:36:44 -0600

    Fix linters

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -4,25 +4,25 @@ class OperationsController < ApplicationController
     @operations = @category.operations.where(author: current_user).order('created_at DESC')
   end
 
-  def new 
+  def new
     @categories = Category.all
     @category = Category.find(params[:category_id])
   end
 
   def create
     @operation = Operation.new name: params[:name], amount: params[:amount],
-      author: current_user
+                               author: current_user
     flash.alert = 'Choose at least one category'
     redirect_back(fallback_location: root_path)
     if @operation.save
-        params[:category].each do |e|
-          category = Category.find(e)
-          @operation.categories << category
-        end
+      params[:category].each do |e|
+        category = Category.find(e)
+        @operation.categories << category
+      end
       redirect_to root_path
     else
       flash.alert = @operation.errors.full_messages
-      redirect_back(fallback_location: root_path) 
+      redirect_back(fallback_location: root_path)
     end
   end
 end
--- spec/models/operation_spec.rb
+++ spec/models/operation_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Operation, type: :model do
   let!(:user) { User.create name: 'David', email: 'david@mail.com', password: 'password' }
-  let!(:wrong_operation) {Operation.create amount: 12}
-  context 'Operation without name' do 
+  let!(:wrong_operation) { Operation.create amount: 12 }
+  context 'Operation without name' do
     it 'should be incorrect' do
       expect(wrong_operation).not_to be_valid
     end
